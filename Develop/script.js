d55ad4e79d4002c265f933db114423476fe4fda2
liliaungurean <lilicikau95@gmail.com>
2022-11-09 11:55:47 -0600

    updated

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+
+  var userInput = window.prompt("Password Lenght")
+//
+  var pasLenght = parseInt(userInput)
+  if (isNaN(pasLenght)) {
+    window.alert("That is not a number! Try again!");
+    return  
+  }
+  if (pasLenght < 8 || pasLenght > 128) {
+    window.alert("password Lengh must be between 8-128 characters ");
+    return
+  }
+
+  var userLower = window.confirm("Include Lowercase?");
+  var userUpper = window.confirm("Include Uppercase?");
+  var userNumbers = window.confirm("Include Numeric?");
+  var userSpecial = window.confirm("Include Special Characters?");
+
+  var lowerList = ["a",	"b",	"c",	"d",	"e",	"f",	"g",	"h",	"i",	"j",	"k",	"l",	"m",	"n",	"o",	"p",	"q",	"r",	"s",	"t",	"u",	"v",	"w",	"x",	"y",	"z",];
+  var upperList = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "X",];
+  var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
+  var specialList = [ "!","#", "$", "%", "&", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~", ]
+
+  console.log
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
